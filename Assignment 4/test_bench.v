23925a596a5837311e5f3ccf1bc04816ac459fe6
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 21:08:36 +0530

    Update test_bench.v

--- Assignment 4/test_bench.v
+++ Assignment 4/test_bench.v
@@ -1,1 +1,21 @@
-
+module up_down_test();
+  reg clk,rst,m;
+  wire [3:0]count;
+  up_down uut(clk,rst,m,count);
+  initial
+    begin
+      clk=0; rst=0; 
+      #25; rst=1;
+    end
+  initial
+    begin
+     m=0; rst=0;
+      #150 m=0; rst=1;
+      #150 m=1; rst=0;
+      #150 m=1;rst=1;
+    end
+  always #5 clk=~clk;
+  initial
+    #1400 $finish;
+endmodule
+      
