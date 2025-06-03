d5ed34bc87d326ffb35a448d8a472c74e0e078ff
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 22:06:11 +0530

    Update test_bench.v

--- Assignment 2/test_bench.v
+++ Assignment 2/test_bench.v
@@ -1,1 +1,15 @@
-
+module fourbit_test();
+  reg [3:0]a,b;
+  reg c0;
+  wire [3:0]s;
+  wire c4;
+  four_fa uut(a,b,c0,s,c4);
+  initial
+    begin
+      a=4'b1011;b=4'b0011;c0=1'b0;#10;
+      a=4'b1011;b=4'b0111;c0=1'b1;#10;
+      a=4'b1111;b=4'b1111;c0=1'b1;#10;
+    end
+  initial
+    #50 $finish;
+endmodule
