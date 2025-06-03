69a6eee8c3525ed2ca3d5b2f2770788f3dd3f381
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 21:16:24 +0530

    Update design.v

--- Assignment 1/design.v
+++ Assignment 1/design.v
@@ -1,1 +1,10 @@
+module mux4to1 (a,sel,y);
+  input [3:0] a;    
+  input [1:0] sel; 
+    output y;     
+assign y = (sel == 2'b00) ? d[0] :
+           (sel == 2'b01) ? d[1] :
+           (sel == 2'b10) ? d[2] :
+                           d[3];
+endmodule
 
