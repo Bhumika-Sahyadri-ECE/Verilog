0ba68c3f604e5564fb789ed0964ebe7141f1a7d2
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 21:01:55 +0530

    Update design.v

--- Assignment 4/design.v
+++ Assignment 4/design.v
@@ -1,1 +1,13 @@
-
+module up_down(clk,rst,m,count);
+  input clk,rst,m;
+  output reg [3:0]count;
+  always@(posedge clk or negedge rst)
+  begin
+    if (!rst)
+      count=0;
+    else if(m)
+      count=count+1;
+    else
+      count=count-1;
+  end
+endmodule
