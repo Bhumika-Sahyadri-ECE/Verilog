0766fd974bec8509b7bf96abc2f7a2201d5dafba
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 22:22:26 +0530

    Update design.v

--- Assigment 3(A)/design.v
+++ Assigment 3(A)/design.v
@@ -1,1 +1,13 @@
-
+module mux2to1 (a,b,sel,y);
+    input a;
+    input b;
+    input sel;
+    output reg y;
+always @(*) 
+  begin
+    case (sel)
+        1'b0: y = a;
+        1'b1: y = b;
+    endcase
+end
+endmodule
