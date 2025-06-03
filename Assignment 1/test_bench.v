f6b15535395fa477a9414a56f5262c833f8a29c6
Bhumika Narayan <152265966+Bhumika-Sahyadri-ECE@users.noreply.github.com>
2025-06-03 21:37:30 +0530

    Update test_bench.v

--- Assignment 1/test_bench.v
+++ Assignment 1/test_bench.v
@@ -1,1 +1,18 @@
-
+module mux4to1_tb();
+  reg  [3:0] a;
+reg  [1:0] sel;
+wire y;
+  mux4to1 uut (a,sel,y);
+initial 
+  begin
+    a = 4'b1010; sel = 2'b00; #10;
+    a = 4'b1010; sel = 2'b01; #10;
+    a = 4'b1010; sel = 2'b10; #10;
+    a = 4'b1010; sel = 2'b11; #10;
+    a = 4'b1101; sel = 2'b00; #10;
+    a = 4'b1101; sel = 2'b01; #10;
+    a = 4'b1101; sel = 2'b10; #10;
+    a = 4'b1101; sel = 2'b11; #10;
+    $finish;
+end
+endmodule
